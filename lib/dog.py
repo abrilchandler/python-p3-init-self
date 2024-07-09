c1090d01ecb93fa76b23c53db75f60810756c315
Chandler Abril <abrilchandler1@gmail.com>
2024-07-08 19:41:26 -0700

    Done

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+
+#1. Dog.__init__ with Name and Breed defaulting to "Mutt"
+#Define a Dog class in lib/dog.py. In the class, define an __init__ method that accepts an argument for the dog's name. That argument should be stored within a self.name attribute.
+#
+#Additionally, Dog.__init__ should accept a second optional argument for the dog's breed stored in an attribute self.breed. When no breed is provided, it should default to "Mutt".
